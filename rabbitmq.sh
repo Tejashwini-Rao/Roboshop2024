e1f5ac538534256c79320ed3e25c281b67309e1b
Tejashwini Rao <polepalle.07@gmail.com>
2024-05-11 21:31:48 +0530

    Pushing all components-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -12,7 +12,9 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 StatusCheck
 
 echo Install RabbitMQ and ErLang
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm rabbitmq-server -y &>>${LOG}
+yum  install erlang -y &>>${LOG}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+yum install rabbitmq-server -y
 StatusCheck
 
 echo Start RabbitMQ Service
