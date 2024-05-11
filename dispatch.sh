314aa0ebcc012861db58b8d246541500ad9e81c9
Tejashwini Rao <polepalle.07@gmail.com>
2024-05-11 22:08:09 +0530

    Pushing all components-shell

--- dispatch.sh
+++ dispatch.sh
@@ -1,6 +1,30 @@
 
+ yum install golang -y
 
-COMPONENT=dispatch
-source common.sh
-GOLANG
 
+1. Add roboshop User
+
+
+# useradd roboshop
+
+
+ Switch to roboshop user and perform the following commands.
+
+
+$ curl -L -s -o /tmp/dispatch.zip https://github.com/roboshop-devops-project/dispatch/archive/refs/heads/main.zip
+$ unzip /tmp/dispatch.zip
+$ mv dispatch-main dispatch
+$ cd dispatch
+$ go mod init dispatch
+$ go get
+$ go build
+
+
+1. Update the systemd file and configure the dispatch service in systemd
+
+```bash
+# mv /home/roboshop/dispatch/systemd.service /etc/systemd/system/dispatch.service
+# systemctl daemon-reload
+# systemctl enable dispatch
+# systemctl start dispatch
+```
